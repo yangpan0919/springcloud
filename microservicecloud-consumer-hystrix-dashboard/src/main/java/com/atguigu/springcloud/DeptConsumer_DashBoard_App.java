bf3b495ded958765ed87704c7a61816b7938bbd1
yp <473075889@qq.com>
2019-03-01 17:29:48 +0800

    1

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/atguigu/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
+
+
