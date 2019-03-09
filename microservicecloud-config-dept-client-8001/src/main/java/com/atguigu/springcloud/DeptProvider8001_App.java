6891f316f459c767cfae8ba20c459e63ef486b03
yp <473075889@qq.com>
2019-03-09 12:58:59 +0800

    1

--- microservicecloud-config-dept-client-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud-config-dept-client-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
-@EnableDiscoveryClient //服务发现
+@EnableDiscoveryClient //服务发现      @EnableEurekaClient 已有该注解
 public class DeptProvider8001_App
 {
     public static void main(String[] args)
